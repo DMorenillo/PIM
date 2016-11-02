14eb5f0a8c2c7b8d881cb0796289662cc21e585b
DMorenillo <guulas@gmail.com>
2016-11-02 09:56:25 +0100

    Añadido MenuContextual

---
+++ MenuContextual/app/src/main/java/com/example/mati/menucontextual/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.mati.menucontextual;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView lblMensaje;
+    private ImageView img;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            //Obtenemos las referencias a los controles
+        lblMensaje = (TextView)findViewById(R.id.lblMensaje);
+            //Asociamos los menús contextuales a los controles
+        registerForContextMenu(lblMensaje);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Alternativa 1
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_inicial, menu);
+        //registerForContextMenu();
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.MnuOpc1:
+                lblMensaje.setText("Opcion 1 pulsada!");
+                return true;
+            case R.id.MnuOpc2:
+                lblMensaje.setText("Opcion 2 pulsada!");;
+                return true;
+            case R.id.MnuOpc3:
+                lblMensaje.setText("Opcion 3 pulsada!");;
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }}
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_contextual, menu);
+    }
+    public boolean onContextItemSelected(MenuItem itemMnuContex) {
+        switch (itemMnuContex.getItemId()) {
+            case R.id.CtxLblOpc1:
+                lblMensaje.setText("Etiqueta: Opcion 1 pulsada!");
+                return true;
+            case R.id.CtxLblOpc2:
+                lblMensaje.setText("Etiqueta: Opcion 2 pulsada!");
+                return true;
+            default:
+                return super.onContextItemSelected(itemMnuContex);
+        }
+    }
+}
